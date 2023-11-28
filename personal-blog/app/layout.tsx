678c454acea8b99b644ea34972164868a4bd2043
Olle Mineur <olle.mineur@gmail.com>
2023-11-28 22:36:11 +0100

    Fixed darkmode

--- personal-blog/app/layout.tsx
+++ personal-blog/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-wrap flex-col content-center h-full min-h-screen max-w-full bg-gradient-to-r from-gray-100 to-gray-300 dark:bg-gradient-to-b dark:from-gray-700 dark:via-gray-900 dark:to-black">
+        <div className="flex flex-wrap flex-col content-center h-full min-h-screen max-w-full bg-gradient-to-r from-gray-100 to-gray-300 dark:bg-gradient-to-b dark:from-gray-700 dark:via-gray-900 dark:to-black dark:text-gray-100">
           <Nav/>
           <div className="min-h-screen w-3/5 mx-auto">
           {children}
--- personal-blog/components/nav/Nav.tsx
+++ personal-blog/components/nav/Nav.tsx
@@ -7,7 +7,7 @@ export default function Nav() {
             ['Projects', '/projects'],
             //['Food Blog', '/food'],
             ].map(([title, url]) => (
-                <a key={title} href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
+                <a key={title} href={url} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900 dark:text-gray-100">{title}</a>
             ))}
         </nav>
     )
