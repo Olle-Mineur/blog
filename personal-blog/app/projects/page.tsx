70e1fc29e68389611aefc1fbce29dfef95051b1b
Olle Mineur <olle.mineur@gmail.com>
2024-12-04 00:28:03 +0100

    Added bachelor thesis and master-planner

--- personal-blog/app/projects/page.tsx
+++ personal-blog/app/projects/page.tsx
@@ -35,6 +35,49 @@ export default function Page() {
             </div>
             <div className="flex flex-wrap flex-col pl-4">
                 <div className="my-2">
+                    <h1 className="font-bold "><a href="https://github.com/Olle-Mineur/liu-master-planner" className="blue-link">LiU Master Planner</a></h1>
+                    <div className="pl-4 my-2">
+                        <p className="text-sm">
+                            Started a project with my friend <a href="https://github.com/CitronMelon" className="blue-link">CitroMelon</a> to make a master course planner for &quot;Civilinjörsprogram&quot; at Linköping University.
+                            The goal is to remove the need for manually making a spreedsheet with all the courses, and instead have a website that can do it for you.
+                            In fall 2024 we have a working scraping tool that can get all the courses from the LiU website, and we are working on the frontend.
+                            Also the backend is in progress, we are using Firebase for the database and authentication. The need of authentication can be removed and instead use json-files.
+                            The website is not yet live.
+                        </p>
+                        <p className="text-sm">
+                            It is built with Next.js, Tailwind CSS and Firebase. We use Puppeteer for web scraping.
+                        </p>
+                        <p className="text-sm">
+                            The source code can be found <a href="https://github.com/Olle-Mineur/liu-master-planner" className="blue-link">here</a>.
+                        </p>
+                        <p className="text-sm">
+                            Started with this website in spring 2024 and is a ongoing project.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-wrap flex-col pl-4">
+                <div className="my-2">
+                    <h1 className="font-bold "><a href="https://urn.kb.se/resolve?urn=urn:nbn:se:liu:diva-205566" className="blue-link">Bachelor thesis</a></h1>
+                    <div className="pl-4 my-2">
+                        <p className="text-sm">
+                            Project with a group of 8 students at Linköping University, the goal was to make a web application to digitalize the construction industry.
+                            The company Bitech was our partner and they wanted to remove the need of papers at construction sites.
+                        </p>
+                        <p className="text-sm">
+                            The web application was made with React, Typescript, Next.js, TailwindCSS. We also used some testing frameworks like Lighthouse, Jest and Cypress.
+                        </p>
+                        <p className="text-sm">
+                            The thesis can be found <a href="https://urn.kb.se/resolve?urn=urn:nbn:se:liu:diva-205566" className="blue-link">here</a>.
+                        </p>
+                        <p className="text-sm">
+                            The project was made in 2024.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-wrap flex-col pl-4">
+                <div className="my-2">
                     <h1 className="font-bold "><a href="https://www.rumfastighet.se/" className="blue-link">Rum Fastighet</a></h1>
                     <div className="pl-4 my-2">
                         <p className="text-sm">
